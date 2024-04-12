0c58ec2442dcedfff1287c7f7b0524b3f0276e6f
Nikita <nikita456nikita@yandex.ru>
2024-04-12 16:51:50 +0300

    fix: fix paths

--- coffee-site/src/components/footer.ts
+++ coffee-site/src/components/footer.ts
@@ -7,7 +7,7 @@ export const footer = () => {
     <div class="address-info">Адрес главного офиса: Минск, ул. Сурганова, 2, Офис 402,406,409</div>
     <div class="call"><a href="tel:+37544521-33-58" class="tel">+375 (44) 521-33-58</a><div class="call-number"><a href="tel:+37544521-33-58">Позвонить </a><i class="material-icons">call</i></div></div>
 </div>
-    <div class="img-wrapper"><img class="map-img" src="Coffee-site/img/map.jpg" alt="office"></div>
+    <div class="img-wrapper"><img class="map-img" src="../Coffee-site/img/map.jpg" alt="office"></div>
     <div class="date">
     <p>Время работы: ПН-ПТ: 09:00-19:00</p>
     <p>Для маркетинга: coffeedelight@mail.ru</p>
